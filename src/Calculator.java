99e7c98d70a2de9299acf0ac2ff573be68ddb3f3
Vaibhavi <vaibhavidhawad@gmail.com>
2022-06-30 17:01:22 +0530

    First Commit

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a=20,b=10,c;
+         System.out.println("Addition"+(a+b));
+         
+         System.out.println("subtraction"+(a-b));
+	}
+
+}
